3707f1677153550609130b3975ab5ab670aaaf4b
erislaniojaco-lab <erislanio.jaco@aluno.ufca.edu.br>
2026-01-09 06:33:01 -0300

    sistema_de_gerenciamento_de_veiculo.py

--- sistema_de_gerenciamento_de_veiculo.py
+++ sistema_de_gerenciamento_de_veiculo.py
@@ -1,17 +1,23 @@
 import json
+# Importação do módulo json
 
 class ManutenivelMixin:
 
     def __init__(self):
+        # Lista que armazena o histórico de manutenções realizadas
         self._historico_manutencao = []
+        # Status atual do objeto (ATIVO ou MANUTENÇÃO)
         self._status = "ATIVO"
 
     @property
     def status(self):
+        # Propriedade para acesso controlado ao status do objeto
         return self._status
 
+     # Criação de um dicionário contendo dados da manutenção
     def registrar_manutencao(self, data, tipo, custo, descricao):
 
+         # Adiciona ao histórico de manutenções
         registro = {"data": data, "tipo": tipo, "custo": custo, "descrição": descricao}
         self._historico_manutencao.append(registro)
 
@@ -37,33 +43,44 @@ class Veiculo(ManutenivelMixin, AbastecivelMixin):
         self._quilometragem = max(0, km)
 
     @property
-    def placa(self): return self._placa
+    def placa(self):
+        return self._placa
     
     @property
-    def modelo(self): return self._modelo
+    # Propriedade para acesso ao atributo modelo
+    def modelo(self):
+        return self._modelo
 
     def __str__(self):
         return f"[{self._status}] {self._modelo} ({self._placa})"
 
 class Carro(Veiculo):
     def __init__(self, placa, modelo, km, consumo_estimado):
+        # Chamada construtor da classe base
         super().__init__(placa, modelo, km, consumo_estimado)
+        # Identificação do tipo de veículo
         self.tipo = "Carro"
+        # Categoria de CNH exigida
         self.cnh_requerida = "B"
 
 class Moto(Veiculo):
     def __init__(self, placa, modelo, km, consumo_estimado):
+        # Chamada construtor da classe base
         super().__init__(placa, modelo, km, consumo_estimado)
+        # Identificação do tipo de veículo
         self.tipo = "Moto"
+        # Categoria de CNH exigida
         self.cnh_requerida = "A"
 
 class Pessoa:  
 
     def __init__(self, nome, cpf):
+        # Atributos privados da pessoa
         self._nome = nome
         self._cpf = cpf
 
 class Motorista(Pessoa):
     pass
+
 
 
